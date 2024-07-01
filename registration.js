0f88fa736f25a63dc278584cc1fad4db428e044e
vaishnavi <shindevaishnavi669@gmail.com>
2024-07-01 22:35:07 +0530

    first commit

--- registration.js
+++ registration.js
@@ -7,7 +7,9 @@ const password2 = document.getElementById('password2');
 form.addEventListener('submit', e => {
     e.preventDefault();
 
-    validateInputs();
+    if (validateInputs()) {
+        alert('Registration successfully');
+    }
 });
 
 const setError = (element, message) => {
@@ -39,34 +41,44 @@ const validateInputs = () => {
     const passwordValue = password.value.trim();
     const password2Value = password2.value.trim();
 
+    let isValid = true;
+
     if(usernameValue === '') {
         setError(username, 'Username is required');
+        isValid = false;
     } else {
         setSuccess(username);
     }
 
     if(emailValue === '') {
         setError(email, 'Email is required');
+        isValid = false;
     } else if (!isValidEmail(emailValue)) {
         setError(email, 'Provide a valid email address');
+        isValid = false;
     } else {
         setSuccess(email);
     }
 
     if(passwordValue === '') {
         setError(password, 'Password is required');
+        isValid = false;
     } else if (passwordValue.length < 8 ) {
-        setError(password, 'Password must be at least 8 character.')
+        setError(password, 'Password must be at least 8 characters.');
+        isValid = false;
     } else {
         setSuccess(password);
     }
 
     if(password2Value === '') {
         setError(password2, 'Please confirm your password');
+        isValid = false;
     } else if (password2Value !== passwordValue) {
-        setError(password2, "Passwords doesn't match");
+        setError(password2, "Passwords don't match");
+        isValid = false;
     } else {
         setSuccess(password2);
     }
 
+    return isValid;
 };
---
+++ staff.js
@@ -0,0 +1,29 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const errorElement = document.getElementById('error');
+    errorElement.textContent = '';
+
+    if (!email && !password) {
+        errorElement.textContent = 'Both fields are empty';
+        return;
+    }
+
+    if (!email) {
+        errorElement.textContent = 'Email is invalid';
+        return;
+    }
+
+    if (email && !password) {
+        errorElement.textContent = 'Password is empty';
+        return;
+    }
+
+    if (password.length < 6) {
+        errorElement.textContent = 'Password length is less than 6';
+        return;
+    }
+
+    alert('Login Successful');
+});
